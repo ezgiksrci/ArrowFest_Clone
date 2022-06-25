2776bc335a4d1645c1011e97cf10c44eb9ee21a2
ezgiksrci <keserci.ezgi@gmail.com>
2022-06-25 18:46:46 +0300

    DOTween eklendi. PlayerController'a eklemeler yapıldı.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] GameObject playerClone;
     [SerializeField] float radius = 1.0f;
+    public List<GameObject> cloneList;
+
+
+    private void Start()
+    {
+        InvokeRepeating("ScaleAnimation", 0f, 1f);
+    }
+
 
     private void OnTriggerExit(Collider other)
     {
         if (other.TryGetComponent(out GateController gateController))
         {
-            Debug.Log(gateController.gateNumber);
-            
-            CreateClonesAroundPoint(gateController.gateNumber, transform.position, radius);
-            radius++;
+            if (gateController.gateNumber > 0)
+            {
+                //Debug.Log(gateController.gateNumber);
+
+                CreateClonesAroundPoint(gateController.gateNumber, transform.position, radius);
+                radius = radius + 0.5f;
+            }
+            else
+            {
+                for (int i = 0; i > gateController.gateNumber; i--)
+                {
+                    Destroy(cloneList[cloneList.Count - 1].gameObject);
+                    cloneList.RemoveAt(cloneList.Count - 1);
+                    if (radius >= 1.5f)
+                    {
+                        radius = radius - 0.5f;
+                    }
+                    else
+                    {
+                        radius = 1.0f;
+                    }
+                }
+            }
         }
     }
 
@@ -39,6 +67,7 @@ public class PlayerController : MonoBehaviour
 
             /* Now spawn */
             var clone = Instantiate(playerClone, spawnPos, Quaternion.identity) as GameObject;
+            cloneList.Add(clone);
 
             /* Make the new object child of the Player */
             clone.transform.parent = player.transform;
@@ -49,5 +78,10 @@ public class PlayerController : MonoBehaviour
             /* Adjust height */
             //clone.transform.Translate(new Vector3(0, clone.transform.localScale.y / 2, 0));
         }
+    }
+
+    private void ScaleAnimation()
+    {
+        transform.DOScale(2, 0.5f).OnComplete(() => transform.DOScale(1, 0.5f));
     }
 }
